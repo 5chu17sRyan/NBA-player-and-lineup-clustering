ef9442c1fc07c9e68e660161e7af38415c9d54c8
Ryan Schultz <ryanschultz2018@yahoo.com>
2021-03-02 17:01:20 -1000

    Cleaned data from or calculated from basketball-reference

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -52,14 +52,18 @@ for(i in 1:nrow(data_2019)){
 # Select unique players and their full (TOT) season data
 data_2019 <- data_2019[unique_player_indices,]
 
+
+
+data_2019$x_of_fga_by_distance_0_3
+str(cleaned_data_2019)
 # Select variables of interest
 cleaned_data_2019 <- data_2019 %>%
   mutate(attempts_0_3 = attempted_fg * x_of_fga_by_distance_0_3) %>%
   mutate(attempts_3_10 = attempted_fg* x_of_fga_by_distance_3_10) %>%
-  mutate(attempts_0_10 = attempts_0_3 + attempts_0_10) %>% #Scoring attempts within 10ft per 100 possessions
+  mutate(attempts_0_10 = attempts_0_3 + attempts_3_10) %>% #Scoring attempts within 10ft per 100 possessions
   mutate(made_0_3 = fg_by_distance_0_3*attempts_0_3) %>%
   mutate(made_3_10 = fg_by_distance_3_10*attempts_3_10) %>%
-  mutate(made_0_10 = made_0_3 + made_0_10) %>%
+  mutate(made_0_10 = made_0_3 + made_3_10) %>%
   mutate(percent_0_10 = made_0_10 / attempts_0_10) %>% #FG% within 10ft
   mutate(attempts_dunk = attempted_fg * dunks_fga) %>% #Dunk attempts per 100 possessions
   mutate(attempts_10_16 = attempted_fg * x_of_fga_by_distance_10_16) %>%
@@ -71,6 +75,8 @@ cleaned_data_2019 <- data_2019 %>%
   mutate(percent_10_3p = made_10_3p / attempts_10_3p) %>% #FG% between 10ft and 3P line
   mutate(attempts_corner = attempted_3p * corner_3s_3pa) %>% #Corner 3 attempts per 100
   select(player, age, attempts_0_10, percent_0_10, attempts_dunk, x_of_fg_ast_d_2p,
-         attempts_10_3p, percent_10_3p, attempts_corner, corner_3s_3p)
+         attempts_10_3p, percent_10_3p, attempts_corner, corner_3s_3p, attempted_3p,
+         percentage_3p, x_of_fg_ast_d_3p, ft_rate, percentage_ft, ast, tov_percentage,
+         blk_percentage, stl_percentage, pf, orb_percentage, drb_percentage)
   
-  
+write.csv(cleaned_data_2019, "C:/Users/ryans/OneDrive/Desktop/Spring 2021/Sports Analytics/Basketball/NBA-player-and-lineup-clustering/cleaned_data_2019.csv")
