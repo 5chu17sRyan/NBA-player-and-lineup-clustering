343f10a23649a74215b38154590d48e71bfbdc39
Shanti <silver1@kenyon.edu>
2021-03-03 16:36:56 -1000

    Merging the Height/Weight Data with the cleaned 2019 variables and uploading the csv.

--- Height and Weight Data.R
+++ Height and Weight Data.R
@@ -16,4 +16,22 @@ HeightWeightData$HEIGHT.in <- HeightWeightData$FEET*12 + HeightWeightData$INCHES
 HeightWeightData <-  HeightWeightData %>%
   select(-c(HEIGHT))
 
-write.csv(HeightWeightData, "C:/Users/Shanti/Desktop/HeightWeightData1.csv")
+#write.csv(HeightWeightData, "C:/Users/Shanti/Desktop/HeightWeightData.csv")
+HeightWeightData <- read.csv("C:/Users/Shanti/Desktop/202110/Sports Analytics/NBA-player-and-lineup-clustering/HeightWeightData.csv")
+cleaned_data_2019 <- read.csv("C:/Users/Shanti/Desktop/202110/Sports Analytics/NBA-player-and-lineup-clustering/cleaned_data_2019.csv")
+
+HeightWeightData <- HeightWeightData %>% 
+  select(-c(X))
+cleaned_data_2019 <- cleaned_data_2019 %>%
+  select(-c(X))
+
+length_name <- NA
+names(HeightWeightData)[1] <- "player"
+for (i in 1:nrow(HeightWeightData)){
+  length_name <- nchar(HeightWeightData$player[i])-1
+  HeightWeightData$player[i] <- substr(HeightWeightData$player[i], 1, length_name)
+}
+
+cleaned_data_2019_w_HW <- merge(cleaned_data_2019, HeightWeightData, by="player", all.x=TRUE)
+write.csv(cleaned_data_2019_w_HW, "C:/Users/Shanti/Desktop/cleaned_data_2019_w_HW.csv")
+cleaned_data_2019_w_HW <- read.csv("C:/Users/Shanti/Desktop/cleaned_data_2019_w_HW.csv")
