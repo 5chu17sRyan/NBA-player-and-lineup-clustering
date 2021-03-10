800a7b67f65a1f621208a1b8a0d72c6bf25cace6
shanti843 <silver1@kenyon.edu>
2021-03-09 16:20:44 -1000

    Updated PCA with player names as columns

--- PCA.R
+++ PCA.R
@@ -9,8 +9,8 @@ all_data <- box_advanced %>%
   select(PersonName, Height, Weight)
 names(all_data)[1] <- "player"
 
-cleaned_data_2019_w_HW <- read.csv("C:/Users/18083/Desktop/202110/Sports Analytics/Basketball/NBA-player-and-lineup-clustering/cleaned_data_2019.csv")
-basketball_data <- cleaned_data_2019_w_HW %>%
+cleaned_data_2019 <- read.csv("C:/Users/18083/Desktop/202110/Sports Analytics/Basketball/NBA-player-and-lineup-clustering/Data/cleaned_data_2019.csv")
+basketball_data <- cleaned_data_2019 %>%
   select(-c(X))
 
 basketball_data <- merge(basketball_data, all_data, by="player", all.x = T)
@@ -45,7 +45,8 @@ basketball_data[,2:24] <- scale(basketball_data[,2:24])
 #imputed PCA
 basketball_data_mean_imp <- basketball_data %>%
   select(-c(player))
-str(basketball_data_mean_imp)
+rownames(basketball_data_mean_imp)<- basketball_data$player
+
 for(i in 1:ncol(basketball_data_mean_imp)) {
   basketball_data_mean_imp[ , i][is.na(basketball_data_mean_imp[ , i])] <- mean(basketball_data_mean_imp[ , i], na.rm = TRUE)
 }
@@ -61,8 +62,6 @@ write.csv(results_mean_imp$rotation, "C:/Users/18083/Desktop/202110/Sports Analy
 
 #saving the PCA. We think it makes most sense to use the mean imputted because same # of components and seems to make sense given that NAs are players who made no attempts
 PCA_mean_imputted <- as.data.frame(results_mean_imp$x)
-player_names <- basketball_data$player
-PCA_mean_imputted <- cbind.data.frame(player_names, PCA_mean_imputted)
 PCA_mean_imputted <- PCA_mean_imputted[,1:6]
 
 write.csv(PCA_mean_imputted, "C:/Users/18083/Desktop/202110/Sports Analytics/Basketball/NBA-player-and-lineup-clustering/PCA_mean_imputted.csv")
